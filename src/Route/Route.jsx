d75aad804b6ff8141c991d659ddd349e4cc06b66
Sojib Das <sojibdas1015@gmail.com>
2026-03-28 16:07:00 +0600

    project setup

---
+++ src/Route/Route.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router';
+import Root from '../Root/Root';
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        Component: Root
+    },
+    {
+        path: '*',
+        element: <div>Page Not Found</div>
+    }
+])
