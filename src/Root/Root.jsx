7464f73a856c4a942440d086282fb91d56278f69
Sojib Das <sojibdas1015@gmail.com>
2026-03-29 20:23:44 +0600

    navbar done with responsive

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navbar from '../Component/Navbar/Navbar';
 
 const Root = () => {
     return (
         <div>
-            
+            <Navbar></Navbar>
         </div>
     );
 };
