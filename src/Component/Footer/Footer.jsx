4627ad39f93b03d2aa47c8978de61f3f0fcdf905
Sojib Das <sojibdas1015@gmail.com>
2026-03-31 14:15:37 +0600

    footer section done

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -5,7 +5,26 @@ import { FaSquareXTwitter, FaSquareYoutube } from 'react-icons/fa6';
 const Footer = () => {
     return (
         <div className='space-y-2 bg-white py-14'>
-            
+            <div className='flex items-center gap-2 justify-center'>
+                <img width={48} src={logo} alt="" />
+                <h2 className='text-3xl font-extrabold'>Phudu</h2>
+            </div>
+            <div className='flex justify-center'>
+                <ul className="gap-4 menu-horizontal p-1">
+                    <li><a>Home</a></li>
+                    <li><a>My-Bookings</a></li>
+                    <li><a>Blogs</a></li>
+                    <li><a>Contact Us</a></li>
+                </ul>
+            </div>
+            <div className='border-1 max-w-[1061px] mx-auto border-[#0F0F0F20]'></div>
+            <div className='text-3xl flex items-center justify-center gap-2'>
+                <FaFacebook />
+                <FaSquareXTwitter />
+                <FaLinkedin />
+                <FaSquareYoutube />
+
+            </div>
         </div>
     );
 };
