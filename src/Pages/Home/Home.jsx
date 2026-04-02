eb7c2d5652dfe0b525b47f221b7b214ce8da290f
Sojib Das <sojibdas1015@gmail.com>
2026-04-02 13:42:08 +0600

    added dynamic title

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,11 +2,17 @@ import React, { useEffect, useState } from 'react';
 import Hero from '../../Component/Hero/Hero';
 import DoctorList from '../../Component/DoctorList/DoctorList';
 import MedicalService from '../../Component/MedicalService/MedicalService';
+import { useLocation } from 'react-router';
 
 const Home = () => {
+    const currentTitle = useLocation()
+
     const [loading, setLoading] = useState(true)
     const [doctorData, setDoctorData] = useState([])
     useEffect(() => {
+        if (currentTitle.pathname == '/') {
+            document.title = 'Phudu | Home'
+        }
         fetch('doctors.json').then(res => res.json()).then(data => {
             setDoctorData(data)
             setLoading(false)
